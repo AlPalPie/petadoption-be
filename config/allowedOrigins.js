c0a16ce4fb4789c786ded7d6f1111211940c3d81
AlPalPie <4087613+AlPalPie@users.noreply.github.com>
2024-01-27 16:34:04 -0800

    fix bug using http instead of https

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 // DEPLOY: modify these origins for deployment
 // removed 'http://localhost:3000' for deployment
 const allowedOrigins = [
-    'http://amazinganimaladoptionagency.onrender.com'
+    'https://amazinganimaladoptionagency.onrender.com'
 ]
 
 module.exports = allowedOrigins
