a9df1911faf20565b1036f509eea58f2d4e687db
AlPalPie <4087613+AlPalPie@users.noreply.github.com>
2024-01-27 17:49:11 -0800

    add back in !origin to corsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,7 @@ const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
     origin: (origin, callback) => {
         // DEPLOY: removed "|| !origin" rom the if condition so that apps like Postman dont have access to the backend
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
